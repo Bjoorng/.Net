0cdc11271b926e4127edd355816c5660e1a759ef
Luca Sernicola <luca.sernicola@gmail.com>
2024-04-16 17:02:59 +0200

    db connection

---
+++ WorkingWithEFCore/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Northwind.EntityModels; // To use Northwind.
+using NorthwindDb db = new();
+WriteLine($"Provider: {db.Database.ProviderName}");
