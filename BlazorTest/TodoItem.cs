3392966ae9739db2fb52eba53b97b88112237c8e
Luca Sernicola <luca.sernicola@gmail.com>
2024-05-13 14:20:59 +0200

    stuff

--- BlazorTest/TodoItem.cs
+++ BlazorTest/TodoItem.cs
@@ -1,6 +1,4 @@
 public class TodoItem {
     public string? Name { get; set; }
     public bool IsDone { get; set; }
-
-
 }
--- BlazorTest/obj/Debug/net8.0/BlazorTest.AssemblyInfo.cs
+++ BlazorTest/obj/Debug/net8.0/BlazorTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5111fbe64bcbddbe4d8349f573b00535305a14ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64857d8d76f4c93fcf18b9145060d54f65b7de05")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
