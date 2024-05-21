70c153f4b969b8ac3c626616e4762b3e28f8e5af
Luca Sernicola <luca.sernicola@gmail.com>
2024-05-21 10:17:27 +0200

    stuff

--- TestAppBlazor/TestAppFrontend/obj/Debug/net8.0/TestAppFrontend.AssemblyInfo.cs
+++ TestAppBlazor/TestAppFrontend/obj/Debug/net8.0/TestAppFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAppFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61983c82e0d3248503067ab964dfca363121698b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b77f85e0be222f65ce179647e2a75fcc44b91c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAppFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAppFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
